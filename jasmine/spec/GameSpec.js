5ffdfc78eb31816f133354f3113c65196b85c632
Nathan Sadler <nathan@sadlerville.com>
2021-09-07 13:29:24 -0400

    begin implementing Game class

--- jasmine/spec/GameSpec.js
+++ jasmine/spec/GameSpec.js
@@ -3,7 +3,31 @@ describe('Game', () => {
     it("sets the players in the game", () => {
       const player1 = new Player('Player 1')
       const player2 = new Player('Player 2')
-      expect(game._players.length).toEqual(2)
+      const player_list = [player1, player2]
+      const game = new Game(player_list)
+      expect(game._players).toEqual(player_list)
+    })
+
+    it("sets the minimum number of players", () => {
+      const game = new Game([], 3)
+      expect(game._minimum_player_count).toEqual(3)
+    })
+  })
+
+  describe('#players', () => {
+    it('returns the list of the players in the game', () => {
+      const player1 = new Player('Player 1')
+      const player2 = new Player('Player 2')
+      const player_list = [player1, player2]
+      const game = new Game(player_list)
+      expect(game.players()).toEqual(player_list)
+    })
+  })
+
+  describe('#minimum_player_count', () => {
+    it("returns the game's minimum number of players", () => {
+      const game = new Game([], 3)
+      expect(game.minimum_player_count()).toEqual(3)
     })
   })
 })
--- jasmine/src/Game.js
+++ jasmine/src/Game.js
@@ -1,3 +1,14 @@
 class Game {
-  
+  constructor(players, minimum_player_count = 2) {
+    this._players = players
+    this._minimum_player_count = minimum_player_count
+  }
+
+  players() {
+    return this._players
+  }
+
+  minimum_player_count() {
+    return this._minimum_player_count
+  }
 }
