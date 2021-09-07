f45caacb2fb51aa0028f9f486b6da5a66af7b7cf
Nathan Sadler <nathan@sadlerville.com>
2021-09-07 11:09:51 -0400

    create player class and add constructor and name() methods

--- jasmine/src/Player.js
+++ jasmine/src/Player.js
@@ -1,22 +1,9 @@
-function Player() {
-}
-Player.prototype.play = function(song) {
-  this.currentlyPlayingSong = song;
-  this.isPlaying = true;
-};
-
-Player.prototype.pause = function() {
-  this.isPlaying = false;
-};
-
-Player.prototype.resume = function() {
-  if (this.isPlaying) {
-    throw new Error("song is already playing");
+class Player {
+  constructor(name) {
+    this._name = name;
   }
 
-  this.isPlaying = true;
-};
-
-Player.prototype.makeFavorite = function() {
-  this.currentlyPlayingSong.persistFavoriteStatus(true);
-};
+  name() {
+    return this._name;
+  }
+}
