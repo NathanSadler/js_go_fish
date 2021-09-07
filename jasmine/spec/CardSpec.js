f6af6877a15bca98ce9db2c168c7b68445683131
Nathan Sadler <nathan@sadlerville.com>
2021-09-07 11:34:36 -0400

    add suit to card constructor

--- jasmine/spec/CardSpec.js
+++ jasmine/spec/CardSpec.js
@@ -5,5 +5,10 @@ describe('Card', () => {
       var card = new Card("4", "S")
       expect(card._rank).toEqual("4")
     })
+
+    it("sets the card's suit", () => {
+      var card = new Card("4", "D")
+      expect(card._suit).toEqual("D")
+    })
   })
 })
--- jasmine/src/Card.js
+++ jasmine/src/Card.js
@@ -1,5 +1,6 @@
 class Card {
   constructor(rank, suit) {
     this._rank = rank;
+    this._suit = suit;
   }
 }
