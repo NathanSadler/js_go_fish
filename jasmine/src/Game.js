fb9b39db740b03f81a284354c9204af641bcaedc
Nathan Sadler <nathan@sadlerville.com>
2021-09-08 12:21:49 -0400

    rearrange methods to be in alphabetical order

--- jasmine/src/Game.js
+++ jasmine/src/Game.js
@@ -2,14 +2,15 @@ class Game {
   constructor(players, minimum_player_count = 2) {
     this._players = players
     this._minimum_player_count = minimum_player_count
-    this._deck = Deck.new()
+    this._deck = new Deck()
+  }
+
+  minimum_player_count() {
+    return this._minimum_player_count
   }
 
   players() {
     return this._players
   }
 
-  minimum_player_count() {
-    return this._minimum_player_count
-  }
 }
