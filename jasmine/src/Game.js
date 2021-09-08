c9c1f00967d5b73eba35060cc8e68a32774fcbd4
Nathan Sadler <nathan@sadlerville.com>
2021-09-08 11:52:08 -0400

    add deck attribute to Game class

--- jasmine/src/Game.js
+++ jasmine/src/Game.js
@@ -2,6 +2,7 @@ class Game {
   constructor(players, minimum_player_count = 2) {
     this._players = players
     this._minimum_player_count = minimum_player_count
+    this._deck = Deck.new()
   }
 
   players() {
