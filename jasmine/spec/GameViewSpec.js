9ff9fd0765ab030c72fb13da449c5582fbf861e8
Nathan Sadler <nathan@sadlerville.com>
2021-09-08 09:02:43 -0400

    What is this change?

--- jasmine/spec/GameViewSpec.js
+++ jasmine/spec/GameViewSpec.js
@@ -1,7 +1,8 @@
 describe('GameView', () => {
-  describe('listing players in a game', () => {
+  describe('displaying players in a game', () => {
+    player_list = [new Player("John"), new Player("Bob")]
+
     it('lists the names of players in the game', () => {
-      player_list = [new Player("John"), new Player("Bob")]
       const view = new GameView(new Game(player_list))
       const container = document.createElement('div')
       document.body.appendChild(container)
