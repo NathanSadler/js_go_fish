174cc5f569f4d47ff8cff0cc43f9bc4896c1333d
Nathan Sadler <nathan@sadlerville.com>
2021-09-08 09:03:02 -0400

    add methods for describing a card

--- jasmine/spec/CardSpec.js
+++ jasmine/spec/CardSpec.js
@@ -11,4 +11,41 @@ describe('Card', () => {
       expect(card._suit).toEqual("D")
     })
   })
+
+  describe('#describe', () => {
+    it("returns a string in the form 'rank' of 'suit's", () => {
+      var card = new Card("4", "D")
+      expect(card.describe()).toEqual('4 of Diamonds')
+    })
+
+    it("uses the name of the rank if the rank it has is just a letter", () => {
+      var card = new Card("Q", "H")
+      expect(card.describe()).toEqual("Queen of Hearts")
+    })
+  })
+
+  describe('#describeRank', () => {
+    it('returns the rank of the card unmodified if it is from 2 to 10', () => {
+      for(let card_rank = 2; card_rank < 11; card_rank++) {
+        card = new Card(card_rank.toString(), "D")
+        expect(card.describeRank()).toEqual(card_rank.toString())
+      }
+    })
+
+    it('returns the name of the rank if it is not a number', () => {
+      card = new Card('Q', 'S')
+      expect(card.describeRank()).toEqual('Queen')
+    })
+  })
+
+  describe('#describeSuit', () => {
+    it("returns the name of the card's suit", () => {
+      const possible_suits = ['C', 'D', 'H', 'S']
+      const possible_suit_names = ['Clubs', 'Diamonds', 'Hearts', 'Spades']
+      for(let i = 0; i < possible_suits.length; i++) {
+        card = new Card("4", possible_suits[i])
+        expect(card.describeSuit()).toEqual(possible_suit_names[i])
+      }
+    })
+  })
 })
--- jasmine/src/Card.js
+++ jasmine/src/Card.js
@@ -3,4 +3,33 @@ class Card {
     this._rank = rank;
     this._suit = suit;
   }
+
+  describeRank() {
+    const named_ranks = {
+      'A' : 'Ace',
+      'J' : 'Jack',
+      'Q' : 'Queen',
+      'K' : 'King'
+    }
+    if(named_ranks.hasOwnProperty(this._rank)) {
+      return named_ranks[this._rank]
+    }
+
+    return this._rank
+  }
+
+  describeSuit() {
+    const suits = {
+      'C' : 'Clubs',
+      'D' : 'Diamonds',
+      'H' : 'Hearts',
+      'S' : 'Spades'
+    }
+    
+    return suits[this._suit]
+  }
+
+  describe() {
+    return `${this.describeRank()} of ${this.describeSuit()}`
+  }
 }
