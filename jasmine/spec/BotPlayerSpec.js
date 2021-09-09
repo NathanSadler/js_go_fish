58b86b271d07c95507eb4da63f2d9bd5492a3f19
Nathan Sadler <nathan@sadlerville.com>
2021-09-09 16:46:13 -0400

    add botPlayer#selectRankToAskFor

--- jasmine/spec/BotPlayerSpec.js
+++ jasmine/spec/BotPlayerSpec.js
@@ -1,3 +1,14 @@
 describe('BotPlayer', () => {
-  
+  let testBot
+
+  beforeEach(() => {
+    testBot = new BotPlayer('Test Bot Player')
+  })
+
+  describe('#selectRankToAskFor', () => {
+    beforeEach(() => testBot._cards = [new Card('7', 'D'), new Card('4', 'H')])
+    it('returns the rank of a randomly selected card in the hand of the bot player', () => {
+      expect(testBot.cards().map(card => card.rank())).toContain(testBot.selectRankToAskFor())
+    })
+  })
 })
--- jasmine/src/BotPlayer.js
+++ jasmine/src/BotPlayer.js
@@ -1,3 +1,10 @@
 class BotPlayer extends Player {
-  
+  constructor(name) {
+    super(name)
+  }
+
+  selectRankToAskFor() {
+    const index = Math.floor(Math.random() * (this.cards().length))
+    return this.cards()[index].rank()
+  }
 }
