9c7897c9c0fa36324e710ef21116b989d268a666
Nathan Sadler <nathan@sadlerville.com>
2021-09-09 16:49:32 -0400

    add submit button to GameView

--- jasmine/src/GameView.js
+++ jasmine/src/GameView.js
@@ -25,13 +25,19 @@ class GameView {
       <form class='turn-form'>
         ${this.game().players().map(player => `<input type='radio' name='player_name' id='${player.name()}' value='${player.name()}'> <label for='${player.name()}'>${player.name()}</label><br>`).join('')}
         ${this.game().players()[0].cards().map(card => `<input type='radio' name='card' id='${card.generateId()}' value=${card.generateId()}> <label for='${card.generateId()}'>${card.describe()}</label><br>`).join('')}
-      </form>
-    
+        <input type="submit" id="submit"></input>
+      </form>    
     `
     const element = document.createElement('div')
     element.innerHTML = markup
+    element.onsubmit = this.onSubmit.bind(this)
     container.appendChild(element)
     return element
+  }
+
+  onSubmit(event) {
+    event.preventDefault()
+    console.log('onSubmit was called')
   }
   
 }
