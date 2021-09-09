a30c7510156fa507174f177a0e6848b8f8b09343
Nathan Sadler <nathan@sadlerville.com>
2021-09-09 16:52:49 -0400

    rearrange tests to be in alphabetical order

--- jasmine/spec/PlayerSpec.js
+++ jasmine/spec/PlayerSpec.js
@@ -11,17 +11,17 @@ describe('Player', () => {
     })
   })
 
-  describe('#name', () => {
-    it("returns the player's name", () => {
-      expect(player.name()).toEqual('Player')
-    })
-  })
-
   describe('#cards', () => {
     it('returns a list of the cards the player has', () => {
       card_list = [new Card("4", "D"), new Card("5", "D"), new Card("6", "D")]
       player._cards = card_list
       expect(player.cards()).toEqual(card_list)
+    })
+  })
+
+  describe('#name', () => {
+    it("returns the player's name", () => {
+      expect(player.name()).toEqual('Player')
     })
   })
 
